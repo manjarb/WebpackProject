6a2eaf602fa89f7fbd424b6e59d67eb9ae9d37dd
varis darasirikul <varis.dara@gmail.com>
2017-04-24 23:33:41 +0700

    Production setup

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
       ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest']
         }),
